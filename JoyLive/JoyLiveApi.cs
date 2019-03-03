69e95dae297bef29821233b344f35e1815fb17b5
Harimurti <hariimurti@gmail.com>
2019-03-03 20:45:59 +0700

    remove image cache -- not working

--- JoyLive/JoyLiveApi.cs
+++ JoyLive/JoyLiveApi.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.IO;
@@ -67,42 +67,5 @@ namespace JoyLive
             nextPage = 1;
             return await GetRoomInfo();
         }
-
-        //public static async Task<string> GetImageCache(ListBoxContext context)
-        //{
-        //    return await Task.Run(() =>
-        //    {
-        //        var filePath = Path.Combine(App.CacheDir, context.Id + ".tmp");
-
-        //        try
-        //        {
-        //            using (WebClient client = new WebClient())
-        //            {
-        //                client.DownloadFile(context.ImageUrl, filePath);
-        //            }
-
-        //            return filePath;
-        //        }
-        //        catch (Exception) { }
-
-        //        try
-        //        {
-        //            var noImg = Properties.Resources.ResourceManager.GetStream("no-image.png");
-        //            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
-        //            {
-        //                for (int i = 0; i < noImg.Length; i++)
-        //                {
-        //                    fileStream.WriteByte((byte)noImg.ReadByte());
-        //                }
-        //                fileStream.Close();
-        //            }
-
-        //            return filePath;
-        //        }
-        //        catch (Exception) { }
-
-        //        return string.Empty;
-        //    });
-        //}
     }
 }
